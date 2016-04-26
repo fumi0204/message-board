6f536f90c5f1108f47af366ad40b22913066b133
Fumitaka Kubo <fumi.new0204@gmail.com>
2016-04-26 11:38:37 +0000

    branch commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new
+    @message = Message.new(message_params)
+    
+    if  @message.save
+      redirect_to root_path , notice:'メッセージを保存しました'
+    else
+    
     @messages = Message.all
+    flash.now[:alert] = "メッセージの保存に失敗しました"
+    render 'index'
   end
+end
   
   def create
     @message = Message.new(message_params)
@@ -12,6 +20,7 @@ class MessagesController < ApplicationController
   
   private
   def message_params
+    ##そもそもこれ何
     params.require(:message).permit(:name, :body)
   end
   
