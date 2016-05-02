332690d36e07d22f71ac5647df92d1fc97afec31
Fumitaka Kubo <fumi.new0204@gmail.com>
2016-05-02 09:09:19 +0000

    Gemfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,19 @@
 class MessagesController < ApplicationController
-  
+  before_action :set_message, only: [:edit, :update ,:destroy]
   def index
       
       @messages = Message.all
       @message = Message.new
-    
-
+  end
+  
+  def edit
   end
 
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
   
   def   create 
     @message = Message.new(message_params)
@@ -17,15 +23,30 @@ class MessagesController < ApplicationController
     else
      @messages = Message.all
      flash.now[:alert] = "メッセージの保存に失敗しました"
-     render "index"
+     render 'index'
     end
   end
 
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
-  def message_params
+   def message_params
     params.require(:message).permit(:name, :body)
+    
+   end
   end
+
+  private
+    def set_message
+     @message = Message.find(params[:id])
+    end
   
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   root 'messages#index'
-  resources :messages , noly:[:create]
+  resources :messages , except:[:index,:new]
+  
+
+
 
 end
 
